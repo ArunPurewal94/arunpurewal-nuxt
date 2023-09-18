a6dbceaf383e9067a0d75d3d67a67a1a8693312c
Arun <arunpurewal94@gmail.com>
2023-09-18 21:16:48 +0100

    Minor Changes

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,5 @@
 import { defineNuxtModule, createResolver, installModule, addComponentsDir } from '@nuxt/kit'
 
-// Module options TypeScript inteface definition
 export interface ModuleOptions {
   css: boolean;
 }
@@ -23,12 +22,14 @@ export default defineNuxtModule<ModuleOptions>({
       /**
        * Here, you specify where your config is.
        * By default, the module have a configPath relative
-       * to the current path, ie the playground !
+       * to the current path, ie the playground
        * (or the app using your module)
        */
       cssPath: options.css ? resolver.resolve("./runtime/assets/css/tailwind.css") : false,
       configPath: resolver.resolve('../tailwind.config'),
     })
+
+    // Allows the components to be used gloabally, in this instance in the playground without importing
 
     await addComponentsDir({
       path: resolver.resolve('./runtime/components'),
